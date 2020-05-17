001c3132b874ce92aac3bbad4171113021c2a7dd
ZePotente <ezeposada@gmail.com>
2020-05-17 15:32:15 -0300

    Agregado paquete de config y lectura.

--- ServerMensajes/Client/src/client/Sistema.java
+++ ServerMensajes/Client/src/client/Sistema.java
@@ -1,13 +1,24 @@
 package client;
 
+import configuracion.Configuracion;
+
+import configuracion.LectorConfiguracion;
+
+import configuracion.NoLecturaConfiguracionException;
+
 import java.io.IOException;
 
 public class Sistema {
     private static Sistema sistema;
+    private static String ARCH_CONFIG = "configuracion.txt";
+    
     private SocketServer server;
     private Agenda agenda;
+    private String NRO_IP_DIRECTORIO = "";
     
-    private Sistema() {
+    private Sistema() throws NoLecturaConfiguracionException {
+        Configuracion config = LectorConfiguracion.leerConfig(Sistema.ARCH_CONFIG);
+        this.NRO_IP_DIRECTORIO = config.getNroIPDirectorio();
         agenda = new Agenda();
         server = new SocketServer(agenda);
         iniciarServer();
@@ -15,7 +26,11 @@ public class Sistema {
     
     public static synchronized Sistema getInstance() {
         if (sistema == null) {
-            sistema = new Sistema();
+            try {
+                sistema = new Sistema();
+            } catch (NoLecturaConfiguracionException e) {
+                System.out.println("Error al leer el archivo de configuracion. Por favor reinicie el programa.");
+            }
         }
         return sistema;
     } 
